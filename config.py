2566f28b902405e9d19ce6cfc460fee72d0b2327
Ceasar <sergey-iline@mail.ru>
2023-05-11 16:17:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_TOKEN = '5443199225:AAHscPjoPwX43_Lit7U9zvkSsZGiX7HNe5g'
+API_TOKEN = ''
 ALLOWED_FORMATS = ["jpeg", "jpg", "png", "gif"]
 MODEL_TYPE = "resnet"
 MODEL_PATH = "Model/yolo8XXXXX.pt"
